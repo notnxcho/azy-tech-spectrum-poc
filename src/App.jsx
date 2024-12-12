176a30ab8d344f4a34496f1c695f0ecb71ebe440
notnxcho <nacholorenzo.650@gmail.com>
2024-12-12 16:19:17 -0300

    left aligned titles

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const techs = [
   {name: 'Armored Vehicle', minScore: 4, maxScore: 7},
   {name: 'Armored Attack Drone', minScore: 3, maxScore: 5},
   {name: 'Infrared telescopic intelligence deployable satellite', minScore: 2, maxScore: 2},
-  {name: 'Infrared telescopic intelligence deployable satellite', minScore: 7, maxScore: 8},
+  {name: 'Infrared telescopic intelligence deployable satellite', minScore: 5, maxScore: 5},
   {name: 'Armored Vehicle', minScore: 4, maxScore: 7},
 ]
 
@@ -22,9 +22,14 @@ function App() {
             {techs.map((tech, index) => {
               return (
                 <div className='tech-row-wrap'>
-                  <div className='spectrum-region' style={{gridColumn: `${tech.minScore} / ${tech.maxScore + 1}`, alignItems: `${tech.minScore > 5 ? 'flex-end' : 'flex-start'}`}}>
-                    <div className="sys-type">Hybrid</div>
-                    <div className='tech-name'>{tech.name}</div>
+                  <div className='spectrum-region' style={{gridColumn: `${tech.minScore} / ${tech.maxScore + 1}`, justifyContent: `${tech.minScore >= 5 ? 'flex-end' : 'flex-start'}`}}>
+                    <div 
+                      className="text-wrap" 
+                      // style={{alignItems: `${tech.minScore >= 5 ? 'flex-end' : 'flex-start'}`}}  
+                    >
+                      <div className="sys-type">Hybrid</div>
+                      <div className='tech-name'>{tech.name}</div>
+                    </div>
                   </div>
                 </div>
               )
